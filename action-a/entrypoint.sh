0c6aeb4350b4fb6a50521ca81c796096f7b240c2
Abdurrahman Shah <47334431+ashah8@users.noreply.github.com>
2019-03-08 13:29:35 -0500

    Update entry

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,3 +2,11 @@
 
 sh -c "echo $*"
 chmod +x action-a/entrypoint.sh
+
+git clone https://github.com/ashah8/hello-world-actions.git
+cd hello-world-actions
+git checkout first-action
+chmod +x action-a/entrypoint.sh
+git add action-a/entrypoint.sh
+git commit -m "make entrypoint script executable"
+git push
